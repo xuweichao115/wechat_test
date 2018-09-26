bed01994f8d3246bb07a687ab38b4c4137d25dd5
poly <poly@DESKTOP-O349LCD>
2018-09-26 18:02:05 +0800

    调整日志模块(未完成)

--- src/main/java/com/aop/LogAnnotationAspect.java
+++ src/main/java/com/aop/LogAnnotationAspect.java
@@ -4,27 +4,29 @@ import java.util.Arrays;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j;
 
 @Aspect
 @Component
-@Slf4j
+@Log4j
 public class LogAnnotationAspect {
 	
-	 @Pointcut("execution(* com.webService.*.*(..))")
+	 @Pointcut("execution(* com.webService.HelloWord.HelloWord2(..))")
 	 public void webLog() {
 	 }
 	 
-	 @Around("webLog()")
-	 public void dobefore(ProceedingJoinPoint joinPoint) {
+	 @Before("webLog()")
+	 public void dobefore(JoinPoint joinPoint) {
 		 	log.info("前置通知：");                     //info ,debug ,warn ,erro四种级别，这里我们注入info级别
 	 
 	        //获取servlet请求对象---因为这不是控制器，这里不能注入HttpServletRequest，但springMVC本身提供ServletRequestAttributes可以拿到
--- src/main/java/com/webService/HelloWord.java
+++ src/main/java/com/webService/HelloWord.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
